46c1f7b54faa123e7307bfbf09eb0871e3ff84a6
yogesh19aggarwal <yogesh.19.aggarwal@gmail.com>
2025-03-05 15:08:46 +0530

    add validation for token file is exist or not

--- services/event.service.js
+++ services/event.service.js
@@ -10,12 +10,15 @@ const credentialsPath = path.join(__dirname, "../config/credentials.json");
 const tokenPath = path.join(__dirname, "../config/token.json");
 
 const credentials = JSON.parse(fs.readFileSync(credentialsPath, "utf8"));
-const token = JSON.parse(fs.readFileSync(tokenPath, "utf8"));
-
-const { client_id, client_secret, redirect_uris } = credentials.installed || credentials.web;
+const { client_id, client_secret, redirect_uris } = credentials.web;
 const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
 
-oAuth2Client.setCredentials(token);
+if (fs.existsSync(tokenPath)) {
+    const token = JSON.parse(fs.readFileSync(tokenPath, "utf8"));
+    oAuth2Client.setCredentials(token);
+} else {
+    console.warn("Warning: token.json not found. Authentication required.");
+}
 
 const calendar = google.calendar({ version: "v3", auth: oAuth2Client });
 
